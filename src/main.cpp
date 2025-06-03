2065152cfbb3da7b39a1ce9206c27fb3c19f36d5
WAD <farhadulkabirr@gmail.com>
2025-06-03 22:57:16 +0600

    finalizing function relocation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "isPrime.h"
 #include "primeIO.h"
 #include <iostream>
+
 int main() {
   int choice;
   std::cout
@@ -30,44 +31,5 @@ int main() {
     break;
   default:
     std::cout << "\nInvalid input try again\n";
-  }
-}
-void printPrimeTilln() {
-  unsigned long long n;
-  std::cout << "\nEnter the value of n: ";
-  std::cin >> n;
-  std::cout << "All the prime numbers until " << n << " are:\n" << "2\n";
-  for (unsigned long long i = 3; n >= i; i = i + 2) {
-    if (isPrime(i))
-      std::cout << i << '\n';
-  }
-}
-void printnthPrime() {
-  int primeCount = 1;
-  unsigned long long x;
-  unsigned long long n;
-  std::cout << "\nEnter the value of n: ";
-  std::cin >> n;
-  for (unsigned long long i = 3; n > primeCount; i = i + 2) {
-    if (isPrime(i)) {
-      primeCount++;
-    }
-    x = i;
-  }
-  std::cout << "the " << n << "-th prime number is " << x << '\n';
-}
-void printFirstnPrimes() {
-  int primeCount = 1;
-  unsigned long long x;
-  unsigned long long n;
-  std::cout << "\nEnter the value of n: ";
-  std::cin >> n;
-  std::cout << "The first " << n << " prime numbers are:\n" << "2\n";
-  for (unsigned long long i = 3; n > primeCount; i = i + 2) {
-    if (isPrime(i)) {
-      std::cout << i << '\n';
-      primeCount++;
-    }
-    x = i;
   }
 }
